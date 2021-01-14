69783c840117ea93d9a5738ddd35efd28df8f2e1
ncaiadas <nunocaiadas25@gmail.com>
2021-01-14 22:23:27 +0000

    added styles to login view

--- birthday_app_vue_cli/src/views/login.vue
+++ birthday_app_vue_cli/src/views/login.vue
@@ -1,19 +1,28 @@
 <template>
 <div>
-  <form @submit.prevent='loginUser()'>
-    <div class="login">
-      <input type="email" placeholder="login" autocomplete="on" v-model="email">
+  <div class="login-card-wrapper">
+    <div class="login-card">
+      <form class="login-card-form" @submit.prevent='loginUser()'>
+        <span class="login-card-title">Login</span>
+        <div class="login">
+          <input type="email" placeholder="login" autocomplete="on" v-model="email">
+        </div>
+        <div class="password">
+          <input type="password" placeholder="password" autocomplete="on" v-model="password">
+        </div>
+        <button class="button" type="submit">Login</button>
+        <div class="error">
+          {{error}}
+          <button v-if="error" @click="hideError()">X</button>
+        </div>
+      </form>
+
     </div>
-    <div class="password">
-      <input type="password" placeholder="password" autocomplete="on" v-model="password">
+
+    <div class="register-card">
+      <span>Need an account? Click here to <router-link to="/register">register</router-link></span>
     </div>
-    <button type="submit">Login</button>
-  </form>
-  <div id="error">
-    {{error}}
-    <button v-if="error" @click="hideError()">X</button>
   </div>
-  <span>Need an account? Click here to <router-link to="/register">register</router-link></span>
 </div>
 </template>
 
@@ -60,5 +69,65 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+ @import './styles/mixins.scss';
 
+.login-card-wrapper {
+  width: 50%;
+  border: 1px solid black;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  height: 400px;
+}
+.login{
+  width: 100%;
+}
+.login-card {
+  flex: 2;
+  align-self: center;
+  border-right: 1px solid black;
+  height: 100%;
+  display: flex;
+  background: #f5f5f5;
+
+  &-form {
+    align-self: center;
+    text-align: left;
+    display: flex;
+    flex-flow: column;
+    width: 100%;
+    input {
+      width: 500px;
+      padding: 15px;
+      margin-bottom: 15px;
+      margin-left: 20px;
+      width: 80%;
+      font-size: 25px;
+    }
+  }
+}
+.login-card-title {
+  margin-left: 20px;
+  font-size: 3em;
+  margin-bottom: 15px;
+}
+.register-card {
+  display: flex;
+  flex: 1;
+  background-color: #3369ff;
+  span {
+    align-self: center;
+    font-size: 20px;
+    color: #f5f5f5;
+  }
+}
+
+.button{
+  @include button;
+  margin-left: 20px;
+  width: 30%;
+}
+.error {
+  margin-left: 20px;
+}
 </style>
